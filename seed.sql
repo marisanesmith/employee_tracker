68376cb1d1e46158c169b916ace901ed018b3cab
marisanesmith <marisa@thisworldtraveled.com>
2021-03-07 10:15:11 -0500

    saved data for tables

--- seed.sql
+++ seed.sql
@@ -16,16 +16,29 @@ INSERT INTO products (flavor, price, quantity)
 VALUES ("vanilla", 2.50, 100);
 
 CREATE TABLE department (
-PRIMARY KEY (id)
-)
-
-CREATE TABLE role (
-PRIMARY KEY (id)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
 )
 
 CREATE TABLE employee (
-PRIMARY KEY (id)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER(1) NOT NULL,
+    manager_id INTEGER(1),
+    PRIMARY KEY (id)
 )
+
+CREATE TABLE role (
+    id INTEGER(1) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INTEGER(1) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+
 
 -- create insert into for each department, role and employee
 -- inner join the 3 tables

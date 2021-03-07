e6cf0ffe5fd5d6522fe4cb75d1233dd31ff3c10e
marisanesmith <marisa@thisworldtraveled.com>
2021-03-07 10:10:44 -0500

    created tables for MySQL

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const mysql = require('mysql')
 const express = require('express')
+const inquirer = require('inquirer')
+
+// inquirer prompts, confirms
+
+// choices of what you would like to do
+
+//add employee
+
+//add job description
+
+//add department
+
+// main menu
--- seed.sql
+++ seed.sql
@@ -4,14 +4,28 @@ CREATE DATABASE jobDB
 
 USE jobDB
 
-CREATE TABLE department (
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  flavor VARCHAR(45) NULL,
+  price DECIMAL(10,2) NULL,
+  quantity INT NULL,
+  PRIMARY KEY (id)
+);
 
+INSERT INTO products (flavor, price, quantity)
+VALUES ("vanilla", 2.50, 100);
+
+CREATE TABLE department (
+PRIMARY KEY (id)
 )
 
 CREATE TABLE role (
-
+PRIMARY KEY (id)
 )
 
 CREATE TABLE employee (
-
+PRIMARY KEY (id)
 )
+
+-- create insert into for each department, role and employee
+-- inner join the 3 tables
